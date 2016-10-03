dbdb9f4f1145b7690ead7049b6634bcbfa0cdd97
v-daabel <d-abell@live.com>
2016-10-03 19:23:57 -0400

    Seed Data

--- MVCdemo/Models/Planet.cs
+++ MVCdemo/Models/Planet.cs
@@ -11,6 +11,7 @@ namespace MVCdemo.Models
         public int PlanetID { get; set; }
         public string Name { get; set; }
         public int Population { get; set; }
+        public string Faction { get; set; }
     }
 
     public class PlanetContext : DbContext
@@ -20,7 +21,22 @@ namespace MVCdemo.Models
         public PlanetContext()
         {
             //Drops and Creates a New DataBase when Changes are made to model
-            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<PlanetContext>());
+            Database.SetInitializer(new MyInitializer());
+        }
+
+        public class MyInitializer :DropCreateDatabaseIfModelChanges<PlanetContext>
+        {
+            protected override void Seed(PlanetContext context)
+            {
+                base.Seed(context);
+                context.Planets.Add(new Planet()
+                {
+                    Name = "Nova Prime",
+                    Population = 846516,
+                    Faction = "Nova Core"
+                });
+                context.SaveChanges();
+            }
         }
     }
 }
--- MVCdemo/PlanetsController.cs
+++ MVCdemo/PlanetsController.cs
@@ -46,7 +46,7 @@ namespace MVCdemo
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "PlanetID,Name,Population")] Planet planet)
+        public ActionResult Create([Bind(Include = "PlanetID,Name,Population,Faction")] Planet planet)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace MVCdemo
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "PlanetID,Name,Population")] Planet planet)
+        public ActionResult Edit([Bind(Include = "PlanetID,Name,Population,Faction")] Planet planet)
         {
             if (ModelState.IsValid)
             {
