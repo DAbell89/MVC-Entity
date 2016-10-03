c85689d33b6d42b3d374741f518ea63d69788d9f
v-daabel <d-abell@live.com>
2016-10-03 19:15:02 -0400

    Views

---
+++ MVCdemo/Models/Planet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVCdemo.Models
+{
+    public class Planet
+    {
+        public int PlanetID { get; set; }
+        public string Name { get; set; }
+        public int Population { get; set; }
+    }
+
+    public class PlanetContext : DbContext
+    {
+        public DbSet<Planet> Planets { get; set; }
+
+        public PlanetContext()
+        {
+            //Drops and Creates a New DataBase when Changes are made to model
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<PlanetContext>());
+        }
+    }
+}
